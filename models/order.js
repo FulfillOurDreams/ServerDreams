d9fee896f93e7dd067bb3f24bd35efc3dcc612b2
SchneiderRachel <r0534108825@gmail.com>
2022-03-22 18:48:43 +0200

    winning controller

--- models/order.js
+++ models/order.js
@@ -1,11 +1,11 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const orderSchema=new mongoose.Schema({
-    item_id:Number,
-    item_name:String,
-    item_description:String,
-    price:Number 
-}) 
+const orderSchema = new mongoose.Schema({
+    item_id: Number,
+    item_name: String,
+    item_description: String,
+    price: Number
+})
 
 module.exports({
     orderSchema
