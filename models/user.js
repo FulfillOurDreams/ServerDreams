b2b4907a5a6b5c2782954b1c8e45b04f3d5ddb7e
SchneiderRachel <r0534108825@gmail.com>
2022-03-22 16:58:11 +0200

    change

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
-const orderSchema = require('./Order').orderSchema
+const { orderSchema } = require('./Order')
 
 const userSchema = new mongoose.Schema({
     user_first_name: String,
     user_last_name: { type: String, validate: /[a-zA-Z]/ },
     user_phone: { type: String, validate: /[0-3]{7-10}/ },
     arr_orders: [orderSchema]
-}) 
+})
 
 const User = mongoose.model("users", userSchema)
 module.exports({
