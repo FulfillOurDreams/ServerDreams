0cede7c801296fd8119ccf4a6608596c904ceee2
michalshveyfel <r0534108825@gmail.com>
2022-03-22 19:29:59 +0200

    add index page

--- models/winning.js
+++ models/winning.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 
 const winnigSchema = new mongoose.Schema({
     user_id: {
-        type:mongoose.Types.ObjectId,
-        ref:"User"
+        type: mongoose.Types.ObjectId,
+        ref: "User"
     },
     item_id: {
-        type:mongoose.Types.ObjectId,
-        ref:"Item"
+        type: mongoose.Types.ObjectId,
+        ref: "Item"
     },
     winning_date: Date
 })
 
 const Winning = mongoose.model("winnings", winnigSchema)
-module.exports({
+module.exports = {
     winnigSchema
-})
+}
