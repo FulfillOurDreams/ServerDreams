a39b53066e543580849c3c561117c5007615d0e2
michalshveyfel <m2122637@gmail.com>
2022-03-22 18:36:40 +0200

    controller winning

---
+++ controller/winning.js
@@ -0,0 +1,49 @@
+const Winning = require("../models/winning").Winning;
+const Item = require("../models/item").Item;
+
+const addwinning = async (req, res) => {
+    
+    try {
+        let win = req.body;
+        let {id,...obj}=win;
+        let w=new Winning(obj);
+        await w.save();
+        return res.send(w);
+    }
+    catch (err) {
+        return res.status(400).send(err)
+    }
+}
+const getWinnerByCode = async (req, res) => {
+    let code = req.params.id;
+    try {
+        let win = await Winning.findById(code);
+        return res.send(win);
+    }
+    catch (err) {
+        return res.status(400).send(err)
+    }
+}
+const winnerbyproductcode = async (req, res) => {
+    let code = req.params.id;
+    try {
+        let win = await Winning.find({item_id:code});
+        return res.send(win);
+    }
+    catch (err) {
+        return res.status(400).send(err)
+    }
+}
+const alluserbyproductcode = async (req, res) => {
+    let code = req.params.id;
+    try {
+        let users = await User.find({}).filter((user)=>{user.arr_orders.find(order=>order.item_id==code)!=-1});
+        return res.send(users);
+    }
+    catch (err) {
+        return res.status(400).send(err)
+    }
+}
+module.exports = {
+    addwinning, getWinnerByCode, winnerbyproductcode, alluserbyproductcode
+}
