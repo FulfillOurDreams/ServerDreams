3d95b6ca9627cdd0308e1bd7eb76050d1c05b501
SchneiderRachel <r0534108825@gmail.com>
2022-04-04 12:51:04 +0300

    fix(): get users by itemId

--- controllers/winning.js
+++ controllers/winning.js
@@ -1,10 +1,11 @@
 const Winning = require("../models/winning").Winning;
 const Item = require("../models/item").Item;
+const { User } = require('../models/user')
 
 const addwinning = async (req, res) => {
-    
+
     try {
-        let win =new Winning(req.body);     
+        let win = new Winning(req.body);
         await win.save();
         return res.send(win);
     }
@@ -25,7 +26,7 @@ const getWinnerByCode = async (req, res) => {
 const winnerbyproductcode = async (req, res) => {
     let code = req.params.id;
     try {
-        let win = await Winning.find({item_id:code});
+        let win = await Winning.find({ item_id: code });
         return res.send(win);
     }
     catch (err) {
@@ -34,11 +35,16 @@ const winnerbyproductcode = async (req, res) => {
 }
 const alluserbyproductcode = async (req, res) => {
     let code = req.params.id;
+    console.log(code)
     try {
-        let users = await User.find({}).filter(user=>{user.arr_orders.find(order=>order.item_id==code)!=-1});
+        let users = await User.find()//.filter(user => { user.arr_orders.find(order => order.item_id == code) != -1 })
+        
+        users.filter(user =>  user.arr_orders.findIndex(order => order.item_id == code) != -1 )
+        console.log(users)
         return res.send(users);
     }
     catch (err) {
+        console.log(err)
         return res.status(400).send(err)
     }
 }
